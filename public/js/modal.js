6364c1e436110e0f93a29ad2a5dd18ebd82ba0b8
saranyan20 <srisaran2000@gmail.com>
2020-08-02 22:08:10 +0530

    Updated ui tweeks
    # added fonts
    # added pics
    # added vendor js files
    # added extra vendor css files

---
+++ public/js/modal.js
@@ -0,0 +1,28 @@
+console.log('modal.js');    
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.querySelector("myBtn");
+
+console.log(btn);
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.addEventListener('click', function() {
+  modal.style.display = "block";
+});
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
---
+++ public/js/video.js
@@ -0,0 +1,8 @@
+var vid = document.getElementById("myvideo");
+vid.playbackRate = 0.5;
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-23581568-13');
