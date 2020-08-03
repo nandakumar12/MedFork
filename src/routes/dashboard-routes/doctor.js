e6320c8f9b5f940864ab64a747674b38ecffc49a
Nandakumar <nandagunasekaran@gmail.com>
2020-08-03 15:03:51 +0530

    Updated doctor and patient routes

--- src/routes/dashboard-routes/doctor.js
+++ src/routes/dashboard-routes/doctor.js
@@ -7,6 +7,7 @@ const path = require("path");
 const { MedicalDetials } = require("../../schemas/medical-details-model");
 const PatientDetails = require("../../schemas/patient-details-model");
 const DoctorRegistrationDetails = require("../../schemas/doctor-registration-schema");
+const HospitalRegistrationDetails = require("../../schemas/hospital-registration-model");
 const model = require("../../schemas/transaction-model");
 
 const { promisify } = require("util");
@@ -15,12 +16,10 @@ const { deepParseJson } = require("deep-parse-json");
 
 const initiateTransaction = require("../../inititate-transaction");
 const { captureRejectionSymbol } = require("events");
-const { RSA_NO_PADDING } = require("constants");
 
 const storage = multer.diskStorage({
   destination: "reports",
   filename: (req, file, cb) => {
-    console.log("filesss came............");
     cb(undefined, file.originalname);
   },
 });
@@ -43,294 +42,88 @@ const upload = multer({
 
 const getHospitalRoute = (blockchain) => {
   const router = express.Router();
-
+  
+  let hospitalDetails;
   let doctorDetails;
 
-  router.post("/", async (req, res) => {
-    // Login credentials verify
-    console.log(req.body.duid, req.body.random_nos, req.body.digital_signature);
-    const isValidSignature = await axios.post(
-      "http://127.0.0.1:8085/verify-sign",
-      {
-        uid: req.body.duid + "_doc",
-        text_data: req.body.random_nos,
-        signature: req.body.digital_signature,
-      }
-    );
-
-    if(!isValidSignature.data.status){
-      res.redirect(`/signature-check-doctor&valid=${false}`);
-    } 
-
-    console.log("signature validity -> ", isValidSignature);
-
-    await DoctorRegistrationDetails.findOne(
-      { doctorRegNo: req.body.duid },
-      (err, doctor) => {
+  router.post("/", async (req,res) => {
+    await HospitalRegistrationDetails.findOne(
+      { _id: req.body.login },
+      (err, hospital) => {
         if (err) {
           console.log("Error during retriving hospital data from DB", err);
           return;
         }
-        doctorDetails = doctor;
-        console.log(doctorDetails);
+        hospitalDetails = hospital;
+        console.log(hospitalDetails);
       }
     );
-    res.render("dashboard-doctor/dashboard", {
-      doctorName: doctorDetails.doctorName,
-      doctorRegNo: doctorDetails.doctorRegNo,
-    });
+    if (
+      req.body.login == hospitalDetails._id &&
+      req.body.password == hospitalDetails.hospitalPassword
+    ) {
+      res.render("dashboard-hospital/dashboard", {
+        hospitalName: hospitalDetails.hospitalName,
+        hospitalRegNo: hospitalDetails._id,
+      });
+    } else {
+      res.redirect(`hospital-login&valid=${false}`);
+    }
   });
 
   router.get("/", (req, res) => {
-    res.render("dashboard-doctor/dashboard", {
-      doctorName: doctorDetails.doctorName,
-      doctorRegNo: doctorDetails.doctorRegNo,
+    res.render("dashboard-hospital/dashboard", {
+      hospitalName: hospitalDetails.hospitalName,
+      hospitalRegNo: hospitalDetails._id,
     });
   });
 
   router.get("/about", (req, res) => {
-    res.render("dashboard-doctor/about", {
-      doctorRegNo: doctorDetails.doctorRegNo,
-      doctorName: doctorDetails.doctorName,
-      doctorYearReg: doctorDetails.doctorYearReg,
-      stateMedicalCouncil: doctorDetails.stateMedicalCouncil,
-      doctorGender: doctorDetails.doctorGender,
-      doctorPnum: doctorDetails.doctorPnum,
+    res.render("dashboard-hospital/about", {
+      _id: hospitalDetails._id,
+      hospitalName: hospitalDetails.hospitalName,
+      hospitalAccrediation: hospitalDetails.hospitalAccrediation,
+      hospitalAddress: hospitalDetails.hospitalAddress,
+      hospitalPnum: hospitalDetails.hospitalPnum,
     });
   });
 
-  router.get("/key-generation", (req, res) => {
-    res.render("key-generation/key-generation");
+  router.get("/doctor-registration", (req,res) => {
+    res.render("dashboard-hospital/doctor-registration");
   });
 
-  router.post("/applicant-details", (req, res) => {
-    axios
-      .get(`http://127.0.0.1:8085/genKeyPair?uuid=${req.body.uuid}`)
-      .then((res) => {
-        console.log(res);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-
-    const verify = {
-      status: "success",
-    };
-    console.log(verify);
-    res.render("details/applicant-details");
-  });
-
-  router.post("/applicant-processing", (req, res) => {
-    const uuid = req.body.uuid;
-    const ruid = req.body.ruid;
-    const name = req.body.name;
-    const gender = req.body.gender;
-    const dob = req.body.dob;
-    const address = req.body.address;
-    const pnum = req.body.pnum;
-
-    axios
-      .get(`http://127.0.0.1:8085/genKeyPair?uuid=${ruid}`)
-      .then((res) => {
-        console.log(ruid + " - ruid key pair generated");
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-
-    const personalDetails = {
-      uuid,
-      ruid,
-      name,
-      dob,
-      gender,
-      address,
-      pnum,
-    };
-
-    new PatientDetails(personalDetails)
-      .save()
-      .then((res) => {
-        console.log(res);
-      })
-      .catch((err) => {
-        console.log(err + "error in saving PatientDetails");
-      });
-    console.log("Personal Data Stored in DB");
-
-    res.render("loader/doctor-loader");
-  });
-
-  router.get("/medical-details", (req, res) => {
-    res.render("details/medical-details");
-  });
-
-  router.post("/processsing", upload.array("reports", 3), async (req, res) => {
-    console.log("inside processsss");
-    const status = req.body.exist;
-    const uuid = req.body.uuid;
-    const duid = req.body.duid;
-    const otherDetails = req.body.details;
-
-    const patientMedicalReport = [];
-    console.log("the files are ", req.files);
-
-    for (const file of req.files) {
-      patientMedicalReport.push(readFile(`./reports/${file.filename}`));
-    }
-    Promise.all(patientMedicalReport)
-      .then((resArr) => {
-        console.log("The final report is :");
-        const medicalReportData = [];
-        resArr.forEach((fileContent) => {
-          console.log("Single content", JSON.parse(fileContent.toString()));
-          medicalReportData.push(JSON.parse(fileContent.toString()));
-        }); //deepParseJson
-        console.log(medicalReportData);
-
-        const prescriptionDetails = [];
-        medicalReportData[1].prescription.forEach((el) => {
-          prescriptionDetails.push(el);
-        });
-
-        const fullMedicalData = {
-          duid,
-          diabetesLevel: [
-            {
-              beforeFasting: medicalReportData[2].sugarData.beforeFasting,
-              afterFasting: medicalReportData[2].sugarData.afterFasting,
-            },
-          ],
-          bpLevel: medicalReportData[0].bpData,
-          prescriptionDetails,
-          otherDetails,
-          hospitalRegNo: medicalReportData[0].hospitalRegNo,
-          hospitalName: medicalReportData[0].hospitalName,
-          date: new Date().toDateString(),
-        };
-
-        console.log("The builded medical report", fullMedicalData);
-
-        if (status == "exist2") {
-          new MedicalDetials({
-            _id: uuid,
-            furtherDetails: [fullMedicalData],
-          })
-            .save()
-            .then()
-            .catch((err) => {
-              console.log(err + "error occured in saving medical details");
-            });
-        } else if (status == "exist1") {
-          MedicalDetials.findById(uuid, (err, patient) => {
-            patient.furtherDetails.push(fullMedicalData);
-            patient
-              .save()
-              .then(() => {
-                console.log(
-                  "database updated with new medical records successfully"
-                );
-              })
-              .catch((err) => {
-                console.log(err + "error occured in saving medical details");
-              });
-          });
-        }
-
-        const medicalDetails = {
-          _id: uuid,
-          duid: fullMedicalData.duid,
-          diabetesLevel: fullMedicalData.diabetesLevel,
-          bpLevel: fullMedicalData.bpLevel,
-          prescriptionDetails: fullMedicalData.prescriptionDetails,
-          otherDetails: fullMedicalData.otherDetails,
-          hospitalRegNo: fullMedicalData.hospitalName,
-          hospitalName: fullMedicalData.hospitalName,
-          date: fullMedicalData.date,
-        };
-        fs.writeFile(
-          path.join(
-            __dirname,
-            "../../../encrypted-files",
-            `${uuid}-medical-report.json`
-          ),
-          JSON.stringify(medicalDetails),
-          (err, data) => {
-            if (err) {
-              console.log("Error while writing medical report");
-              return;
-            }
-            console.log("The medical data is generated");
-          }
-        );
-      })
-      .catch((err) => {
-        console.log("Error while parsing the medical record", err);
-      });
-
-    console.log("Initiated new transaction");
-
-    initiateTransaction(uuid, duid, blockchain);
-    const response = {
-      message: "Transaction will be added to The Blockchain",
-    };
-
-    console.log(response);
-
-    res.render("loader/doctor-loader");
-  });
-
-  router.get("/shared-data", (req, res) => {
-    res.render("dashboard-doctor/shared-data", {
-      error: " ",
+  router.post("/doctor-processing", async (req,res) => {
+    new DoctorRegistrationDetails({
+      doctorName: req.body.doctorName,
+      doctorRegNo: req.body.doctorRegNo,
+      doctorYearReg: req.body.doctorYearReg,
+      stateMedicalCouncil: req.body.stateMedicalCouncil,
+      doctorGender: req.body.doctorGender,
+      doctorPnum: req.body.doctorPnum
+    }).save()
+    .then((res) => {
+      console.log("The doctor data is",res);
+    })
+    .catch((err) => {
+      console.log(err + "error in saving Doctor Details");
     });
+    console.log("After catch");
+
+    // generateDocKey(duid);
+    await axios.get(`http://127.0.0.1:8085/genKeyPair?uuid=${req.body.doctorRegNo + "_doc"}`);
+
+    res.render("loader/hospital-processing");
   });
 
-  router.post("/data-processing", (req, res) => {
-    const patientUID = req.body.patient_uuid;
-    const doctorUID = req.body.doctor_uuid;
-    const dataHash = req.body.data_hash;
-    console.log("-> ", patientUID, doctorUID, dataHash);
-
-    for (let blockNo = blockchain.chain.length - 1; blockNo >= 0; blockNo--) {
-      console.log(blockchain.chain);
-      const transactions = blockchain.chain[blockNo].transactions;
-      for(let idx = 0 ; idx < transactions.length ; idx++){
-        if (transactions[idx].transactionType == "policy") {
-          const currentTransaction = blockchain.chain[blockNo].transactions[idx];
-          if (
-            currentTransaction.senderUID == patientUID && 
-            currentTransaction.receiverUID == doctorUID && 
-            currentTransaction.dataHash == dataHash && 
-            currentTransaction.remove == true
-          ) {
-            res.render("dashboard-doctor/shared-data", {
-              error: "Patient revoked access!!",
-            });
-            return console.log("Sorry the paient has revoked access");
-        }
-      }
-    }
-  }
-    
-
-    axios
-      .post("http://127.0.0.1:8099/reEncrypt", {
-        s_uid: patientUID,
-        r_uid: doctorUID,
-        public_key_digitalsign: dataHash,
-      })
-      .then((res) => {
-        console.log("result -> ", res);
-        if (res.data.status == "success") {
-          console.log(
-            "responce from datasharing -> ",
-            res.data.decryptedMessage
-          );
-        }
+  router.get("/doctor-list", (req,res) => {
+    DoctorRegistrationDetails.find({}).lean().exec((err, doctor) => {
+      doctorDetails = doctor
+      console.log("doctor details",doctorDetails);
+      res.render("dashboard-hospital/doctor-list", {
+        doctorDetails,
       });
-    res.render("loader/doctor-loader");
-  });
+    });
+  })
 
   return router;
 };
--- src/routes/dashboard-routes/patient.js
+++ src/routes/dashboard-routes/patient.js
@@ -2,8 +2,12 @@ const express = require("express");
 const axios = require("axios");
 const PatientDetails = require("../../schemas/patient-details-model");
 const { MedicalDetials } = require("../../schemas/medical-details-model");
+const { PatientModel } = require("../../schemas/transaction-model");
 const fs = require("fs");
 const crypto = require("crypto");
+const { MerkleTree } = require("merkletreejs");
+const SHA256 = require("crypto-js/sha256");
+const e = require("express");
 
 const getPatientRoute = (blockchain) => {
   const router = express.Router();
@@ -23,9 +27,9 @@ const getPatientRoute = (blockchain) => {
       }
     );
 
-    if(!isValidSignature.data.status){
+    if (!isValidSignature.data.status) {
       res.redirect(`/signature-check&valid=${false}`);
-    } 
+    }
 
     console.log("signature validity -> ", isValidSignature);
 
@@ -83,7 +87,7 @@ const getPatientRoute = (blockchain) => {
   router.get("/", (req, res) => {
     res.render("dashboard-patient/dashboard", {
       patientName,
-      patientUID
+      patientUID,
     });
   });
 
@@ -153,68 +157,155 @@ const getPatientRoute = (blockchain) => {
     });
   });
 
-  router.post("/patient", (req, res) => {
+  router.post("/patient", async (req, res) => {
     let remove = 0;
     if (req.body.exist === "add") {
       remove = 0;
     } else {
       remove = 1;
     }
-    console.log("chain -> ", blockchain.chain[1].transactions);
-    let capsule = null;
-    for (const block of blockchain.chain) {
-      for (const transaction of block.transactions) {
-        if (transaction.ipfsFileHash == req.body.ipfsHash) {
-          capsule = transaction.capsule;
-          break;
+
+    const typeOfPolicy = req.body.policy;
+
+    const time = req.body.time;
+    console.log(time);
+    let myDate = time.split("-");
+    myDate = myDate[1] + "/" + myDate[2] + "/" + myDate[0];
+    console.log(new Date(myDate).getTime());
+
+    if (typeOfPolicy == "multifile") {
+      let transactionsDetails = null;
+      PatientModel.findOne({ uuid: patientUUID })
+        .exec()
+        .then((data) => {
+          if (!data) {
+            console.log("patient not found");
+            return;
+          }
+          let result = [];
+          transactionsDetails = data.transactions;
+          console.log(transactionsDetails);
+          for (
+            let transaction = 0;
+            transaction < transactionsDetails.length;
+            transaction++
+          ) {
+            console.log(
+              "db time -> ",
+              transactionsDetails[transaction].time,
+              new Date(myDate).getTime()
+            );
+            console.log(
+              transactionsDetails[transaction].time > new Date(myDate).getTime()
+            );
+            if (
+              transactionsDetails[transaction].time > new Date(myDate).getTime()
+            ) {
+              result.push(transactionsDetails[transaction].ipfsFileHash);
+            }
+          }
+          console.log("transacion model ->", result);
+
+          const leaves = result.map((x) => SHA256(x));
+          const tree = new MerkleTree(leaves, SHA256);
+          MerkleTree.print(tree);
+          const root = tree.getRoot().toString("hex");
+
+          axios
+            .post("http://127.0.0.1:8099/newPolicy", {
+              s_uid: patientUUID,
+              r_uid: req.body.duid,
+              data_hash: root,
+              public_key_digitalsign: crypto
+                .createHash("sha256")
+                .update(root)
+                .digest("hex"),
+              capsule: " ",
+              remove,
+            })
+            .then((res) => {
+              console.log(res);
+              if (res.data.status == "success" && remove == 0) {
+                console.log("sending new policy tranaction");
+                blockchain.newPolicyTransaction({
+                  senderUID: patientUUID,
+                  receiverUID: req.body.duid,
+                  dataHash: root,
+                  capsule: "multi file policy transaction",
+                  remove: false,
+                });
+              } else if (remove == 1) {
+                console.log("revoking the policy");
+                const chainLength = blockchain.newPolicyRemoveTransaction({
+                  senderUID: patientUUID,
+                  receiverUID: req.body.duid,
+                  dataHash: root,
+                });
+                console.log(chainLength);
+              }
+            })
+            .catch((err) => {
+              console.log("Error during creating new policy", err);
+            });
+        });
+    } else {
+      console.log("chain -> ", blockchain.chain[1].transactions);
+      let capsule = null;
+      for (const block of blockchain.chain) {
+        for (const transaction of block.transactions) {
+          if (transaction.ipfsFileHash == req.body.ipfsHash) {
+            capsule = transaction.capsule;
+            break;
+          }
         }
       }
-    }
-    console.log("capsule -> ", capsule);
-    if (capsule) {
-      axios
-        .post("http://127.0.0.1:8099/newPolicy", {
-          s_uid: patientUUID,
-          r_uid: req.body.duid,
-          data_hash: req.body.ipfsHash,
-          public_key_digitalsign: crypto
-            .createHash("sha256")
-            .update(capsule)
-            .digest("hex"),
-          capsule,
-          remove,
-        })
-        .then((res) => {
-          console.log(res);
-          if (res.data.status == "success" && remove == 0) {
-            console.log("sending new policy tranaction");
-            blockchain.newPolicyTransaction({
-              senderUID: patientUUID,
-              receiverUID: req.body.duid,
-              dataHash: req.body.ipfsHash,
-              capsule,
-              remove: false,
-            });
-          }
-          else if (remove == 1) {
-            console.log("revoking the policy");
-            const chainLength = blockchain.newPolicyRemoveTransaction({
-              senderUID: patientUUID,
-              receiverUID: req.body.duid,
-              dataHash: req.body.ipfsHash,
-            });
-            console.log(chainLength);
-          }
-        })
-        .catch((err) => {
-          console.log("Error during creating new policy", err);
-        });
+
+      console.log("capsule -> ", capsule);
+      if (capsule) {
+        axios
+          .post("http://127.0.0.1:8099/newPolicy", {
+            s_uid: patientUUID,
+            r_uid: req.body.duid,
+            data_hash: req.body.ipfsHash,
+            public_key_digitalsign: crypto
+              .createHash("sha256")
+              .update(capsule)
+              .digest("hex"),
+            capsule,
+            remove,
+          })
+          .then((res) => {
+            console.log(res);
+            if (res.data.status == "success" && remove == 0) {
+              console.log("sending new policy tranaction");
+              blockchain.newPolicyTransaction({
+                senderUID: patientUUID,
+                receiverUID: req.body.duid,
+                dataHash: req.body.ipfsHash,
+                capsule,
+                remove: false,
+              });
+            } else if (remove == 1) {
+              console.log("revoking the policy");
+              const chainLength = blockchain.newPolicyRemoveTransaction({
+                senderUID: patientUUID,
+                receiverUID: req.body.duid,
+                dataHash: req.body.ipfsHash,
+              });
+              console.log(chainLength);
+            }
+          })
+          .catch((err) => {
+            console.log("Error during creating new policy", err);
+          });
+      }
     }
 
     res.render("dashboard-patient/dashboard", {
       patientName: patientDetails.name,
       patientUID: patientDetails.uuid,
     });
+    
   });
 
   return router;
